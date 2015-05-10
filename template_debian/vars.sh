2bc86f17b0afdcb1c40687efdac2da49cf787e43
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-05-10 05:29:36 +0200

    Add --force-unsafe-io to speedup installation
    If something fails, we don't case - it is really easy to just recreate
    chroot (or template image).

--- template_debian/vars.sh
+++ template_debian/vars.sh
@@ -35,7 +35,7 @@ fi
 # ------------------------------------------------------------------------------
 # apt-get configuration options
 # ------------------------------------------------------------------------------
-APT_GET_OPTIONS="-o Dpkg::Options::="--force-confnew" --yes"
+APT_GET_OPTIONS="-o Dpkg::Options::=--force-confnew -o Dpkg::Options::=--force-unsafe-io --yes"
 
 containsFlavor 'no-recommends' && {
     APT_GET_OPTIONS+=" -o APT::Install-Recommends=0  -o APT::Install-Suggests=0" 
